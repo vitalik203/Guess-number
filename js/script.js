656af11390098f9c3cb75e44954c5e116cd96d06
VitaliiZaluskii <vzaluskiy23@gmail.com>
2024-02-19 17:23:26 +0200

    more beautiful version

--- js/script.js
+++ js/script.js
@@ -7,10 +7,6 @@ const result = document.querySelector(".main__col2_result");
 const scoreBox = document.querySelector(".main__col2_score");
 const guessedNum = document.querySelector(".main__guessed_number");
 
-again.addEventListener("click", function () {
-  location.reload();
-});
-
 function generateNum() {
   return Math.floor(Math.random() * 25 + 1);
 }
@@ -19,38 +15,52 @@ let score = 20;
 scoreBox.innerHTML = `Кількість очок: ${score}`;
 let genValue = generateNum();
 
+again.addEventListener("click", function () {
+  genValue = generateNum();
+  score = 20;
+  scoreBox.innerHTML = `Кількість очок: ${score}`;
+  input.value = "";
+  result.innerText = "";
+  guessedNum.innerHTML = "";
+});
+
 console.log(genValue);
 
 check.addEventListener("click", function () {
-  let clicks = 0;
-  let trying = Number(input.value);
-  console.log(typeof trying);
-  clicks++;
-  if (trying === genValue) {
-    result.innerText = "Ура, ви вгадали!!!";
-    result.innerHTML = `Спроб залишилося: ${
-      score - clicks < 0 ? 0 : score - clicks
-    }`;
-    guessedNum.innerHTML = `<h2 style="display: inline-block">${genValue}</h2> - це число яке було задумане`;
-    check.removeEventListener(
-      "click",
-      function () {
-        input.value = "";
-      },
-      false
-    );
-  } else if (trying < genValue) {
-    result.innerText = "Замало";
-  } else if (trying > genValue) {
-    result.innerText = "Забагато";
-  }
-  scoreBox.innerHTML = "";
-  if (trying !== genValue) {
-    score--;
-    scoreBox.innerHTML = `Кількість очок: ${score}`;
-  }
-  if (score < 1) {
-    result.innerText = "Гра програна!";
+  if (input.value === "") {
     scoreBox.innerHTML = "";
+    scoreBox.innerHTML = "Введіть будь ласка число від 1 до 25";
+  } else {
+    let clicks = 0;
+    let trying = Number(input.value);
+    console.log(typeof trying);
+    clicks++;
+    if (trying === genValue) {
+      result.innerText = "Ура, ви вгадали!!!";
+      result.innerHTML = `Спроб залишилося: ${
+        score - clicks < 0 ? 0 : score - clicks
+      }`;
+      guessedNum.innerHTML = `<h2 style="display: inline-block">${genValue}</h2> - це число яке було задумане`;
+      check.removeEventListener(
+        "click",
+        function () {
+          input.value = "";
+        },
+        false
+      );
+    } else if (trying < genValue) {
+      result.innerText = "Замало";
+    } else if (trying > genValue) {
+      result.innerText = "Забагато";
+    }
+    scoreBox.innerHTML = "";
+    if (trying !== genValue) {
+      score--;
+      scoreBox.innerHTML = `Кількість очок: ${score}`;
+    }
+    if (score < 1) {
+      result.innerText = "Гра програна!";
+      scoreBox.innerHTML = "";
+    }
   }
 });
